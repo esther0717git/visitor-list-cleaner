f338c60b6d4aab5f29e6c4601084189d97926961
esther0717git <liangwy@sea.com>
2025-07-15 15:58:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -370,7 +370,8 @@ st.markdown(
     Please be reminded to go through the Clarity Gate prior to submission, and ensure that all visitor and shipment details are complete and accurate to prevent rescheduling due to clarification.
 
     **Note:**  
-    The Clarity Gate operates on the GOFAI system, which relies on explicitly programmed rules and logic. Although its validation accuracy can reach up to 95%, we strongly recommend that you thoroughly review all information before submission.
+    The Clarity Gate operates on the GOFAI system, which relies on explicitly programmed rules and logic. 
+    Although its validation accuracy can reach up to 95%, we strongly recommend that you thoroughly review all information before submission.
 
     Thank you for your cooperation.
     """
