c5e15be96c8f36bddaccf3413685da3c1ed28a74
esther0717git <liangwy@sea.com>
2025-08-11 14:35:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.expander("Why is Data Integrity Important?"):
 with open("sample_template.xlsx", "rb") as f:
     sample_bytes = f.read()
 st.download_button(
-    label="⬇Download Sample Template",
+    label="🌟 Download Template",
     data=sample_bytes,
     file_name="sample_template.xlsx",
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
